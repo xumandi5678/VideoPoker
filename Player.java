d69d2c78ffb80929cd8795527663fe6dac1a481d
Mandi Xu <73205412+xumandi5678@users.noreply.github.com>
2022-06-08 13:36:10 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -8,72 +8,78 @@ import java.util.Collections;
 
 public class Player {
 	
-		
 	private ArrayList<Card> hand; // the player's cards
 	private double bankroll;
-    private double bet;
-
-	// you may choose to use more instance variables
-		
+    	private double bet;
+	
+	/* Creates Player here */
 	public Player(){		
-	    // create a player here
-        hand = new ArrayList<>();
-        bankroll = 1000.0;
-        bet = 0.0;
+        	hand = new ArrayList<>();
+        	bankroll = 1000.0;
+        	bet = 0.0;
 	}
-
+	
+	/* Adds a Card to 
+	 * the Player's hand */ 
 	public void addCard(Card c){
-	    // add the card c to the player's hand
-        hand.add(c);
+        	hand.add(c);
 	}
-
+	
+	/* Removes a Card from 
+	 * the Player's Hand */
 	public void removeCard(Card c){
-	    // remove the card c from the player's hand
-        hand.remove(c); 
+        	hand.remove(c); 
         }
-		
-    public void bets(double amt){
-        // player makes a bet
-        bet = bet + amt;
-        bankroll = bankroll - bet;
-    }
-
-    public void winnings(double odds){
-        //	adjust bankroll if player wins
-        bankroll = bankroll + (bet * odds);
-    }
-
-    public double getBankroll(){
-        // return current balance of bankroll
-        return bankroll;
-    }
-
-    // you may wish to use more methods here
+	
+	/* Player places a bet in 
+	 * an amount given 
+	 */
+    	public void bets(double amt){
+        	bet = bet + amt;
+        	bankroll = bankroll - bet;
+    	}
+	
+	/* Adjusts the Bankroll if 
+	 * the player wins to include earnings 
+	 */ 
+    	public void winnings(double odds){
+        	bankroll = bankroll + (bet * odds);
+    	}
+	
+	/* Returns the current balance
+	 * of the bankroll 
+	 */ 
+    	public double getBankroll(){
+        	return bankroll;
+    	}
     
-    public ArrayList<Card> sortHand() { 
-        // arranges the hand in order for comparison later
-        Collections.sort(hand);
-        return hand;
-    }
-    
-    public String toString() {
-        sortHand();
-       // displays the hand to the player 
-       String currentHand = "Current hand: \n";
-       for(Card c : hand) { 
-           currentHand = currentHand + c.toString() + "\n";
-       }
-       return currentHand;
-       }
-    
-    public ArrayList<Card> getHand() {
-        // returns the hand for access
-        return hand;
-    }
-    
-    public void clearBet() { 
-        bet = 0.0;
-    }
+	/* Arranges the hand in order 
+	 * for comparison later 
+	 */
+    	public ArrayList<Card> sortHand() { 
+        	Collections.sort(hand);
+        	return hand;
+    	}
+    	
+	/* Displays the hand to the player */
+    	public String toString() {
+        	sortHand();
+        	String currentHand = "Current hand: \n";
+        	for(Card c : hand) { 
+           		currentHand = currentHand + c.toString() + "\n";
+       		}
+       		return currentHand;
+	}
+    	
+	/* Returns the Hand to the Player */
+    	public ArrayList<Card> getHand() {
+        	return hand;
+    	}
+    	
+	/* Sets the Bet to 0 for new game */
+    	public void clearBet() { 
+        	bet = 0.0;
+    	}
     
 }
 
